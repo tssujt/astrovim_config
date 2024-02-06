074dc7768bc0bc5c4afdfb2641e8466f783f5da3
tssujt <github@tssujt.xyz>
2024-02-06 11:49:25 +0800

    Introduce CopilotC-Nvim/CopilotChat.nvim

--- plugins/community-completion.lua
+++ plugins/community-completion.lua
@@ -22,6 +22,34 @@ return {
     config = function() require("copilot_cmp").setup() end,
   },
   {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
+      debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
+      disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
+    },
+    build = function()
+      vim.notify "Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim."
+    end,
+    event = "VeryLazy",
+    keys = {
+      { "<leader>ce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
+      { "<leader>ct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
+      {
+        "<leader>cv",
+        ":CopilotChatVisual",
+        mode = "x",
+        desc = "CopilotChat - Open in vertical split",
+      },
+      {
+        "<leader>cx",
+        ":CopilotChatInPlace<cr>",
+        mode = "x",
+        desc = "CopilotChat - Run in-place code",
+      },
+    },
+  },
+  {
     "onsails/lspkind.nvim",
     opts = function(_, opts)
       return require("astronvim.utils").extend_tbl(opts, {
